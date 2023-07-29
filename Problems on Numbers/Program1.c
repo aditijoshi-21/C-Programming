e714749fe27cfa320b5609d6de453208a83af54e
Aditi Joshi <99011088+chiks21@users.noreply.github.com>
2023-07-29 12:54:08 +0530

    Update Program1.c

--- Problems on Numbers/Program1.c
+++ Problems on Numbers/Program1.c
@@ -1,1 +1,36 @@
+//Accept number from user and display its multiplication of factors
 
+#include<stdio.h>
+
+int MultFact(int iNo)
+{
+    int iCnt = 0;
+    int iMul = 1;
+
+    for(iCnt=1;iCnt<=(iNo/2);iCnt++)
+    {
+        if(iNo%iCnt==0)
+        {
+            iMul= iMul*iCnt;
+        }
+    }
+
+    return iMul;
+
+}
+
+int main()
+{
+    int iValue=0;
+    int iRet=0;
+
+    printf("Enter number: ");
+    scanf("%d",&iValue);
+
+    iRet = MultFact(iValue);
+
+    printf("Multiplication of factors of number: %d",iRet);
+
+
+    return 0;
+}
