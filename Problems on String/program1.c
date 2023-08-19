31521883feb560efeefde972b99cec8dab0ce2ac
Aditi Joshi <99011088+aditijoshi-21@users.noreply.github.com>
2023-08-19 16:45:40 +0530

    Create program1.c

---
+++ Problems on String/program1.c
@@ -0,0 +1,48 @@
+//Accept character from user and check whether it is alphabet or not
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkAlpha(char ch)
+{
+    BOOL bFlag = FALSE;
+
+    if((ch>='a') && (ch<='z') || (ch>='A') && (ch<='Z'))
+    {
+        bFlag=TRUE;
+    }
+    else
+    {
+        bFlag = FALSE;
+    }
+
+    return bFlag;
+}
+
+int main()
+{
+    char cValue= '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character\n");
+    scanf("%c",&cValue);
+
+    bRet = ChkAlpha(cValue);
+
+    if(bRet==TRUE)
+    {
+        printf("It is Cahracter");
+    }
+
+    else
+    {
+        printf("It is not Character");
+    }
+
+    return 0;
+}
+
